389c2db3e355af31dc2c079e6a315b4bc280e9e7
Jonathan <Jonathanraes@gmail.com>
2015-05-07 11:25:59 +0200

    Ball jumping up with vel 10.0 and than falling on a plane

--- src/main/java/com/mycompany/mavenproject1/App.java
+++ src/main/java/com/mycompany/mavenproject1/App.java
@@ -9,6 +9,7 @@ import com.jme3.app.SimpleApplication;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.collision.shapes.SphereCollisionShape;
+import com.jme3.bullet.control.GhostControl;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.AnalogListener;
@@ -21,16 +22,26 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Line;
+import com.jme3.scene.shape.Quad;
 import com.jme3.scene.shape.Sphere;
 
 public class App extends SimpleApplication {
 	
 	private BulletAppState bulletAppState;
 	
+	Material line_mat;
+	Material ball_mat;
+	Material floor_mat;
+	
+	private RigidBodyControl ball_phy;
+	private Sphere sphere;
+	private Box floor;
+	private RigidBodyControl floor_phy;
+	
+	
     public AnalogListener analogListener = new AnalogListener() {
         public void onAnalog(String name, float value, float tpf) {
             System.out.println(name + " = " + value);
-            
         }
     };
 
@@ -42,38 +53,79 @@ public class App extends SimpleApplication {
     @Override
     public void simpleInitApp() {
    // 	inputManager.setCursorVisible( true );
-    	flyCam.setEnabled(false);
-    	
-        Sphere sphere = new Sphere(3, 3, 3);
-        Geometry geom = new Geometry("Sphere", sphere);
+    	/* Set up physics */
+    	bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
+        bulletAppState.setDebugEnabled(true);
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-        geom.setLocalTranslation(new Vector3f(1.0f, 1.0f, 1.0f));
+//    	flyCam.setEnabled(false);
         
         // Add a physics sphere to the world
 //        Node physicsSphere = PhysicsTestHelper.createPhysicsTestNode(assetManager, new SphereCollisionShape(1), 1);
 //        physicsSphere.getControl(RigidBodyControl.class).setPhysicsLocation(new Vector3f(3, 6, 0));
 //        rootNode.attachChild(physicsSphere);
 //        getPhysicsSpace().add(physicsSphere);
-//        
-        Line line = new Line(new Vector3f(5.0f, 0f, 0f), new Vector3f(-5.0f, 0f, 0f));
-        Geometry linegeom = new Geometry("Line", line);
         
-        Material linemat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        linemat.setColor("Color", ColorRGBA.Red);
-        linegeom.setMaterial(linemat);
         
-        rootNode.attachChild(geom);
-        rootNode.attachChild(linegeom);
+//        Quad ground = new Quad(4f, 4f);
+//        Geometry groundgeom = new Geometry("Ground", ground);
+//        Material groundmat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+//        groundmat.setColor("Color", ColorRGBA.DarkGray);
+//        groundgeom.setMaterial(groundmat);
         
+//        rootNode.attachChild(groundgeom);
+       
+//        rootNode.attachChild(linegeom);
+        initWorld();
+        getPhysicsSpace().add(ball_phy);
         inputManager.addMapping("MouseMovement",
                 new MouseAxisTrigger(MouseInput.AXIS_X, false), 
                 new MouseAxisTrigger(MouseInput.AXIS_X, true));
         
         inputManager.addListener(analogListener, "MouseMovement");
 
+    }
+    
+    public void initWorld(){
+    	/* physics control */
+    	SphereCollisionShape shape = new SphereCollisionShape(2f);
+    	ball_phy = new RigidBodyControl(shape, 1f);
+    	ball_phy.setLinearVelocity(new Vector3f(0.0f, 10.0f, 0.0f));
+    	Node node = new Node("physics controlled ball");
+    	node.addControl(ball_phy);
+    	
+    	/* Line */    	
+    	Line line = new Line(new Vector3f(15.0f, -1f, 0f), new Vector3f(-15.0f, -1f, 0f));
+        Geometry linegeom = new Geometry("Line", line);
+        Material linemat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        linemat.setColor("Color", ColorRGBA.Red);
+        linegeom.setMaterial(linemat);
+        
+        /* Ball */
+        sphere = new Sphere(20, 20, 2);
+        Geometry geom = new Geometry("Sphere", sphere);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+        geom.setLocalTranslation(new Vector3f(1.0f, 5.0f, 1.0f));
+        
+        geom.addControl(ball_phy);
+        
+        /* Floor */
+        floor = new Box(10f, 0.1f, 5f);
+        Geometry floor_geom = new Geometry("Floor", floor);
+        floor_mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        floor_geom.setMaterial(floor_mat);
+        floor_phy = new RigidBodyControl(0.0f);
+        floor_geom.addControl(floor_phy);
+        getPhysicsSpace().add(floor_phy);
+        
+        rootNode.attachChild(floor_geom);
+
+		node.attachChild(geom);
+		node.attachChild(linegeom);
+
+		rootNode.attachChild(node);
     }
 
     @Override
