dd5acce7eefa25632ae1c9610c0a1993f2b8f9f9
olivier-dikken <garou9999@gmail.com>
2015-05-13 10:35:17 +0200

    travis ci opencv test

--- src/main/java/com/mycompany/mavenproject1/opencvTemplate.java
+++ src/main/java/com/mycompany/mavenproject1/opencvTemplate.java
@@ -5,6 +5,8 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
 public class opencvTemplate {
+	
+	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     public static void main(String[] args) {
         System.out.println("Welcome to OpenCV " + Core.VERSION);
@@ -12,5 +14,4 @@ public class opencvTemplate {
         Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("m = " + m.dump());
     }
-
 }
