2d909e24de8f0dc7274867f8214f8f22951097aa
SDumasy <SDumasy@wlan-145-94-197-189.wlan.tudelft.nl>
2015-05-14 13:18:12 +0200

    Checkstyled the test classes

--- src/test/java/com/funkydonkies/w4v3/obstacles/MovingBoxTest.java
+++ src/test/java/com/funkydonkies/w4v3/obstacles/MovingBoxTest.java
@@ -12,7 +12,7 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 
 /**
- * This class
+ * This class test the moving box.
  * @author SDumasy
  *
  */
@@ -22,54 +22,78 @@ public class MovingBoxTest {
 	private static Material mat;
 	private static PhysicsSpace psySpace;
 	private static Node rootNode;
+	private static final double SPEED = 0.01;
+	private static final double MOVEDOWN = 1.01;
+	
+	private static final int WIDTH = 2;
+	private static final int HEIGHT = 4;
+	private static final int DEPTH = 1;
 
 
+	/**
+	 * The setup, needed to instantiate and mock objects.
+	 */
 	@BeforeClass
 	public static void setup() {
 		obF = new ObstacleFactory();
-		mBox = (MovingBox) obF.makeObstacle("MOVINGBOX", 2, 4, 1);
+		mBox = (MovingBox) obF.makeObstacle("MOVINGBOX", WIDTH, HEIGHT, DEPTH);
 		mat = mock(Material.class);
 		psySpace = mock(PhysicsSpace.class); 
 		rootNode = new Node();
 	}
 	
+	/**
+	 * Tests if the box is succesfully attached to the rootnode.
+	 */
 	@Test
-	public void drawTest(){		
+	public final void drawTest() {		
 		mBox.draw(mat, psySpace, rootNode);
 		assertEquals(rootNode.getChildren().size(), 1);
 	}
 	
+	/**
+	 * tests if the box moves up correctly.
+	 */
 	@Test
-	public void moveUpTest(){
-		Geometry geom = mBox.getGeometry();
-		Vector3f vec = geom.getLocalTranslation();
+	public final void moveUpTest() {
+		final Geometry geom = mBox.getGeometry();
+		final Vector3f vec = geom.getLocalTranslation();
 		mBox.draw(mat, psySpace, rootNode);
 		mBox.move();
-		Vector3f vec2 = geom.getLocalTranslation();
-		vec2.setY((float)(vec.getY() - 0.01));
+		final Vector3f vec2 = geom.getLocalTranslation();
+		vec2.setY((float) (vec.getY() - SPEED));
 		assertEquals(vec, vec2);	
 	}
 	
+	/**
+	 * test if the move direction changes accordingly.
+	 */
 	@Test
-	public void changeMoveUpTest(){
-		Geometry geom = mBox.getGeometry();
-		Vector3f vec = geom.getLocalTranslation();
-		while(vec.getY() <= mBox.getHeight())
+	public final void changeMoveUpTest() {
+		final Geometry geom = mBox.getGeometry();
+		final Vector3f vec = geom.getLocalTranslation();
+		while (vec.getY() <= mBox.getHeight()) {
 			mBox.move();
-		Vector3f vec2 = geom.getLocalTranslation();
-		vec2.setY((float)(vec.getY() - 1.01));
+		}
+		
+		final Vector3f vec2 = geom.getLocalTranslation();
+		vec2.setY((float) (vec.getY() - MOVEDOWN));
 		assertEquals(vec, vec2);	
 	}
 
+	/**
+	 * test if the box moves down correctly.
+	 */
 	@Test
-	public void moveDownTest(){
-		Geometry geom = mBox.getGeometry();
-		Vector3f vec = geom.getLocalTranslation();
-		while(vec.getY() <= mBox.getHeight())
+	public final void moveDownTest() {
+		final Geometry geom = mBox.getGeometry();
+		final Vector3f vec = geom.getLocalTranslation();
+		while (vec.getY() <= mBox.getHeight()) {
 			mBox.move();
+		}
 		mBox.move();
-		Vector3f vec2 = geom.getLocalTranslation();
-		vec2.setY((float)(vec.getY() - 1));
+		final Vector3f vec2 = geom.getLocalTranslation();
+		vec2.setY((float) (vec.getY() - 1));
 		assertEquals(vec, vec2);	
 	}
 
--- src/test/java/com/funkydonkies/w4v3/obstacles/ObstacleFactoryTest.java
+++ src/test/java/com/funkydonkies/w4v3/obstacles/ObstacleFactoryTest.java
@@ -5,24 +5,38 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * This test tests the obstacle factory.
+ * @author SDumasy
+ *
+ */
 public class ObstacleFactoryTest {
 	
 	private static ObstacleFactory obF;
-
+	
+	/**
+	 * setup a obstacle factory.
+	 */
 	@BeforeClass
 	public static void setup() {
 		obF = new ObstacleFactory();
 	}
 	
+	/**
+	 * Test if the movingbox can be instantiated correctly.
+	 */
 	@Test
-	public void instantiateMovingBox(){
-		MovingBox mBox = (MovingBox) obF.makeObstacle("MOVINGBOX", 2, 4, 1);
+	public final void instantiateMovingBox() {
+		final MovingBox mBox = (MovingBox) obF.makeObstacle("MOVINGBOX", 2, 4, 1);
 		assertNotEquals(mBox, null);
 	}
 	
+	/**
+	 * Test if the not valid objects aren't instantiated.
+	 */
 	@Test
-	public void instantiateNotExisting(){
-		Obstacle notValidOb = obF.makeObstacle("NOTVALID", 2, 4, 1);
+	public final void instantiateNotExisting() {
+		final Obstacle notValidOb = obF.makeObstacle("NOTVALID", 2, 4, 1);
 		assertEquals(notValidOb, null);
 	}
 
