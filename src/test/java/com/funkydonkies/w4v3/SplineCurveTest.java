6ef4b1120ede72011ebffb54f377702a9375c0cf
Danilodum14 <D.Dumeljic-1@student.tudelft.nl>
2015-05-16 23:45:42 +0200

    fixed checkstyle error in AppTest and SplineCurveTest

--- src/test/java/com/funkydonkies/w4v3/SplineCurveTest.java
+++ src/test/java/com/funkydonkies/w4v3/SplineCurveTest.java
@@ -1,84 +1,112 @@
 package com.funkydonkies.w4v3;
 
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.jme3.asset.AssetManager;
-import com.jme3.asset.DesktopAssetManager;
 import com.jme3.bullet.PhysicsSpace;
-import com.jme3.bullet.collision.PhysicsCollisionEvent;
-import com.jme3.bullet.collision.shapes.SphereCollisionShape;
 import com.jme3.material.Material;
 import com.jme3.math.Spline.SplineType;
-import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
 
+/**
+ * Testing SplineCurve.
+ *
+ */
 public class SplineCurveTest {
 	private static Vector3f[] vecs;
-	static SplineCurve sp;
-	static SplineCurve sp1;
-	static Material mat;
-	static PhysicsSpace psy;
-	static AssetManager ass;
+	private static SplineCurve sp;
+//	private static SplineCurve sp1;
+	private static Material mat;
+	private static PhysicsSpace psy;
 	
+	/** Initializes class variables once.
+	 * 
+	 * @throws Exception -
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		vecs = new Vector3f[3];
-    	vecs[0] = new Vector3f(0,0,0);
-    	vecs[1] = new Vector3f(2,4,0);
-    	vecs[2] = new Vector3f(4,1,0);
-    	sp = new SplineCurve(SplineType.CatmullRom, vecs, (float)0.5, true);
-    	sp1 = mock(SplineCurve.class);
+		final Vector3f v0 = new Vector3f(0, 0, 0);
+		final Vector3f v1 = new Vector3f(2, 4, 0);
+		final Vector3f v2 = new Vector3f(4, 1, 0);
+		final int size = 3;
+		vecs = new Vector3f[size];
+    	vecs[0] = v0;
+    	vecs[1] = v1;
+    	vecs[2] = v2;
+    	
+    	final float curveTension = 0.5f;
+    	sp = new SplineCurve(SplineType.CatmullRom, vecs, curveTension, true);
+//    	sp1 = mock(SplineCurve.class);
 		mat = mock(Material.class);
 		psy = mock(PhysicsSpace.class);
-		ass = mock(AssetManager.class);
 	}
 
+	/**
+	 * Test getTwoVectors.
+	 */
 	@Test
-	public void getBeginTwoVectorsTest(){
-		Vector3f[] veccies = sp.getTwoVectors(0, 0);
+	public void getBeginTwoVectorsTest() {
+		final Vector3f[] veccies = sp.getTwoVectors(0, 0);
 		assertEquals(vecs[0], veccies[0]);
 		assertEquals(vecs[1], veccies[1]);
 	}
 	
+	/**
+	 * Test getTwoVectors.
+	 */
 	@Test
-	public void getMiddleTwoVectorsTest(){
-		Vector3f[] veccies = sp.getTwoVectors(1, 0);
+	public void getMiddleTwoVectorsTest() {
+		final Vector3f[] veccies = sp.getTwoVectors(1, 0);
+		final float val = 0.01f;
 		assertEquals(vecs[1], veccies[0]);
-		assertEquals(sp.interpolate(0.01f, 1, null), veccies[1]);
+		assertEquals(sp.interpolate(val, 1, null), veccies[1]);
 	}
 	
+	/**
+	 * Test getTwoVectors.
+	 */
 	@Test
-	public void getEndTwoVectorsTest(){
-		Vector3f[] veccies = sp.getTwoVectors(1, 0.5);
-		assertEquals(sp.interpolate(0.5f, 1, null), veccies[0]);
-		assertEquals(sp.interpolate(0.51f, 1, null), veccies[1]);
+	public void getEndTwoVectorsTest() {
+		final Vector3f[] veccies = sp.getTwoVectors(1, 0.5);
+		final float val0 = 0.5f;
+		final float val1 = 0.51f;
+		assertEquals(sp.interpolate(val0, 1, null), veccies[0]);
+		assertEquals(sp.interpolate(val1, 1, null), veccies[1]);
 	}
 	
+	/**
+	 * Test drawBox.
+	 */
 	@Test
-	public void drawBoxTest(){
-		Node n = new Node();
-		Node node = sp.drawBox(vecs, mat, psy, n);
+	public void drawBoxTest() {
+		final Node n = new Node();
+		final Node node = sp.drawBox(vecs, mat, psy, n);
 		assertEquals(node.getChildren().size(), 1);
 	} 
 	
+	/**
+	 * Test drawCurve.
+	 */
 	@Test
-	public void drawCurveTest(){
-		Node rootNode = new Node();
+	public void drawCurveTest() {
+		final Node rootNode = new Node();
 		sp.drawCurve(rootNode, mat, psy);
 		assertEquals(rootNode.getChildren().size(), 1);
 	}
 	
+//	/**
+//	 * Test loopThroughSpline.
+//	 */
 //	@Test
-//	public void loopThroughSplineTest(){
-//		Node n = mock(Node.class);
+//	public void loopThroughSplineTest() {
+//		final Node n = mock(Node.class);
 //		sp1.loopThroughSpline(n, psy, mat);
-//		verify(sp1, times(1000000)).drawBox(vecs, mat, psy, n);
+//		final int amount = 1000000;
+//		verify(sp1, times(amount)).drawBox(vecs, mat, psy, n);
 //	}
 	
 }
