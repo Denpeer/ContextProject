506f2cb7473712f4d93fbc4e162c75baecf41c59
sdumasy <stephan_dumasy@icloud.com>
2015-05-29 15:40:08 +0200

    Tested curve package

--- src/test/java/com/funkydonkies/w4v3/curve/SplineCurveTest.java
+++ src/test/java/com/funkydonkies/w4v3/curve/SplineCurveTest.java
@@ -9,12 +9,17 @@ import org.junit.Test;
 import com.jme3.bullet.PhysicsSpace;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.material.Material;
+import com.jme3.math.Spline.SplineType;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 
 public class SplineCurveTest {
 	static SplineCurve sCurve;
 	static Material mat;
 	static PhysicsSpace phys;
 	static RigidBodyControl rigid;
+	static SplineCurve sp;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -22,11 +27,45 @@ public class SplineCurveTest {
 		mat = mock(Material.class);
 		phys = mock(PhysicsSpace.class);
 		rigid = mock(RigidBodyControl.class);
+		sp = new SplineCurve(SplineType.CatmullRom, true);
+	}
+
+	
+	@Test
+	public void testConstruct() {
+		assertFalse(sp.getCurvePoints() == null);
+	}
+	
+	@Test
+	public void drawCurvetest() {
+		Node node = new Node();
+		sp.drawCurve(mat, phys, rigid, node);
+		assertTrue(node.getChildren().size() == 1);
+	}
+	
+	@Test
+	public void incrementPointsTest(){
+		Vector3f curveP = new Vector3f();
+		curveP = sp.getCurvePoints()[0];
+		sp.incrementPoints();
+		assertEquals(sp.getCurvePoints()[0], curveP);
 	}
 
 	@Test
-	public void drawCurvetest() {
-		
+	public void decrementPointsTest(){
+		Vector3f curveP = new Vector3f();
+		curveP = sp.getCurvePoints()[0];
+		sp.decrementPoints();
+		assertEquals(sp.getCurvePoints()[0], curveP);
 	}
-
+	
+	@Test
+	public void getGeometryTest(){
+		Geometry geom;
+		sp.getGeometry();
+		geom = sp.getGeometry();
+		assertFalse(geom == null);
+	}
+	
+	
 }
