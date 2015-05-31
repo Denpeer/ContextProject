7cc432e3f2860bd160b8259931fd488ff897182f
Jonathan <Jonathanraes@gmail.com>
2015-05-31 14:20:38 +0200

    added bridge, wont build anymore?

--- src/test/java/com/funkydonkies/controllers/CurveStateTest.java
+++ src/test/java/com/funkydonkies/controllers/CurveStateTest.java
@@ -1,13 +1,15 @@
 package com.funkydonkies.controllers;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.any;
+
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +19,7 @@ import com.funkydonkies.gamestates.CameraState;
 import com.funkydonkies.gamestates.CurveState;
 import com.funkydonkies.gamestates.PlayState;
 import com.funkydonkies.w4v3.App;
+import com.funkydonkies.w4v3.Bridge;
 import com.funkydonkies.w4v3.curve.SplineCurve;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.bullet.PhysicsSpace;
@@ -40,10 +43,11 @@ public class CurveStateTest {
 	private CurveState spyCurveState;
 	private SplineCurve spySplinaCurve;
 	private Geometry geom;
+	private Bridge bridge;
 	
 	@Before
 	public void setUp() {
-		
+		bridge = mock(Bridge.class);
 		curveState = new CurveState();
 		spyCurveState = spy(curveState);
 		sManager = mock(AppStateManager.class);
@@ -60,11 +64,14 @@ public class CurveStateTest {
 		when(playState.getPhysicsSpace()).thenReturn(physicsSpace);
 		when(sManager.getState(CameraState.class)).thenReturn(camState);
 		when(sManager.getState(PlayState.class)).thenReturn(playState);
-		doReturn(material).when(spyCurveState).initializeMaterial();
+		when(camState.getBridge()).thenReturn(bridge);
+		when(bridge.isBgSet()).thenReturn(true);
 		when(app.getRootNode()).thenReturn(node);
 		when(spyCurveState.initializeSplineCurve()).thenReturn(spySplinaCurve);
 		when(spyCurveState.makeRigidBodyControl()).thenReturn(control);
 		when(spySplinaCurve.getGeometry()).thenReturn(geom);
+		when(bridge.getControlPoints()).thenReturn(getTestPoints());
+		doReturn(material).when(spyCurveState).initializeMaterial();
 		doNothing().when(spySplinaCurve).drawCurve(
 				material, physicsSpace, control, node);
 	}
@@ -81,18 +88,30 @@ public class CurveStateTest {
 		
 	}
 	
+	private float[] getTestPoints() {
+		float[] points = new float[curveState.getAmountOfControlPoints()];
+		Arrays.fill(points, 480);
+		final int bottomX = 1;
+		final int topX = curveState.getAmountOfControlPoints() - 1;
+		for (int i = bottomX; i < topX; i++) { // TESTING CODE
+			final int scale = 10;
+			points[i] = i * scale;
+		}
+		return points;
+	}
+	
 	@Test
 	public void updateTest() {
 		spyCurveState.initialize(sManager, app);
 		spyCurveState.update(0.01f);
 		spyCurveState.setUpdateEnabled(true);
+		spyCurveState.toggleCameraEnabled();
 		verify(node).detachChildNamed("curve");
 		verify(spySplinaCurve).drawCurve(material, physicsSpace, control, node);
 		verify(geom).removeControl(any(RigidBodyControl.class));
 
 		verify(geom, never()).removeControl(control);
 		verify(spyCurveState, never()).makeRigidBodyControl();
-		
 		spyCurveState.update(0.01f);
 		
 //		verify(spyCurveState).makeRigidBodyControl();
