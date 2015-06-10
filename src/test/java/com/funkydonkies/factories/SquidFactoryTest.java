423b48e4f850b945a72a136c2d91023d8ffa3085
olivier-dikken <garou9999@gmail.com>
2015-06-10 15:55:58 +0200

    tested squid thunder and yeti factories

--- src/test/java/com/funkydonkies/factories/SquidFactoryTest.java
+++ src/test/java/com/funkydonkies/factories/SquidFactoryTest.java
@@ -1,19 +1,92 @@
 package com.funkydonkies.factories;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import com.funkydonkies.controllers.SquidControl;
+import com.funkydonkies.gamestates.PlayState;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetKey;
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.material.MatParam;
+import com.jme3.material.Material;
+import com.jme3.material.MaterialDef;
+import com.jme3.scene.Geometry;
+
+/**
+ * Test the squidfactory.
+ * @author Olivier Dikken
+ *
+ */
 public class SquidFactoryTest {
 
+	private AppStateManager asmMock;
+	private SimpleApplication saMock;
+	private AssetManager amMock;
+	private SquidFactory sf;
+	private SquidFactory sfMock;
+	private MaterialDef mdMock;
+	private MatParam mpMock;
+	private PlayState psMock;
+	private PhysicsSpace physMock;
+
+	/**
+	 * setup for tests. Mock all that is needed.
+	 * @throws Exception exception
+	 */
+	@SuppressWarnings("unchecked")
 	@Before
 	public void setUp() throws Exception {
+		asmMock = Mockito.mock(AppStateManager.class);
+		saMock = Mockito.mock(SimpleApplication.class);
+		sf = new SquidFactory();
+		sfMock = Mockito.mock(SquidFactory.class);
+		amMock = Mockito.mock(AssetManager.class);
+		mdMock = Mockito.mock(MaterialDef.class);
+		mpMock = Mockito.mock(MatParam.class);
+		psMock = Mockito.mock(PlayState.class);
+		physMock = Mockito.mock(PhysicsSpace.class);
+		Mockito.when(saMock.getAssetManager()).thenReturn(amMock);
+		Mockito.when(amMock.loadAsset(Mockito.any(AssetKey.class))).thenReturn(mdMock);
+		Mockito.when(mdMock.getMaterialParam(Mockito.any(String.class))).thenReturn(mpMock);
+		Mockito.when(asmMock.getState(PlayState.class)).thenReturn(psMock);
+		Mockito.when(psMock.getPhysicsSpace()).thenReturn(physMock);
+		Mockito.doNothing().when(physMock).add(Mockito.any(SquidControl.class));
 	}
 
+	/**
+	 * test make object.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMakeObject() {
+		assertTrue(sf.makeObject(asmMock, saMock) instanceof Geometry);
+		assertTrue(sf.makeObject(asmMock, saMock).getControl(0) instanceof SquidControl);
+	}
+
+	/**
+	 * test make squid.
+	 */
+	@Test
+	public void testMakeSquid() {
+		sf.makeObject(asmMock, saMock);
+		assertTrue(sf.makeSquid() instanceof Geometry);
+		assertTrue(sf.makeSquid().getControl(0) instanceof SquidControl);
+		sfMock.makeSquid();
+		Mockito.verify(saMock.getAssetManager(), Mockito.times(1));
+	}
+
+	/**
+	 * test get squid material.
+	 */
+	@Test
+	public void testGetSquidMaterial() {
+		final Object mat = sf.getSquidMaterial(amMock);
+		assertTrue(mat instanceof Material);
 	}
 
 }
--- src/test/java/com/funkydonkies/factories/YetiFactoryTest.java
+++ src/test/java/com/funkydonkies/factories/YetiFactoryTest.java
@@ -1,19 +1,104 @@
 package com.funkydonkies.factories;
 
-import static org.junit.Assert.*;
+
+
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import com.funkydonkies.controllers.SquidControl;
+import com.funkydonkies.controllers.YetiControl;
+import com.funkydonkies.gamestates.PlayState;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetKey;
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.PhysicsSpace;
+import com.jme3.material.MatParam;
+import com.jme3.material.Material;
+import com.jme3.material.MaterialDef;
+import com.jme3.material.RenderState;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+
+/**
+ * Testing the yetifactory class.
+ * @author Olivier Dikken
+ *
+ */
 public class YetiFactoryTest {
 
+	private AppStateManager asmMock;
+	private SimpleApplication saMock;
+	private AssetManager amMock;
+	private YetiFactory yf;
+	private MaterialDef mdMock;
+	private MatParam mpMock;
+	private PlayState psMock;
+	private PhysicsSpace physMock;
+	private Node rootNodeMock;
+	private Material matMock;
+	private RenderState renderStateMock;
+
+	/**
+	 * setup for tests. Mock all that is needed.
+	 * @throws Exception exception
+	 */
+	@SuppressWarnings("unchecked")
 	@Before
 	public void setUp() throws Exception {
+		asmMock = Mockito.mock(AppStateManager.class);
+		saMock = Mockito.mock(SimpleApplication.class);
+		yf = new YetiFactory();
+		Mockito.mock(YetiFactory.class);
+		amMock = Mockito.mock(AssetManager.class);
+		mdMock = Mockito.mock(MaterialDef.class);
+		mpMock = Mockito.mock(MatParam.class);
+		psMock = Mockito.mock(PlayState.class);
+		physMock = Mockito.mock(PhysicsSpace.class);
+		rootNodeMock = Mockito.mock(Node.class);
+		matMock = Mockito.mock(Material.class);
+		renderStateMock = Mockito.mock(RenderState.class);
+		Mockito.when(saMock.getAssetManager()).thenReturn(amMock);
+		Mockito.when(amMock.loadAsset(Mockito.any(AssetKey.class))).thenReturn(mdMock);
+		Mockito.when(mdMock.getMaterialParam(Mockito.any(String.class))).thenReturn(mpMock);
+		Mockito.when(asmMock.getState(PlayState.class)).thenReturn(psMock);
+		Mockito.when(psMock.getPhysicsSpace()).thenReturn(physMock);
+		Mockito.doNothing().when(physMock).add(Mockito.any(SquidControl.class));
+		Mockito.when(saMock.getRootNode()).thenReturn(rootNodeMock);
+		Mockito.when(rootNodeMock.getUserData(Mockito.any(String.class))).thenReturn(matMock);
+		Mockito.when(matMock.clone()).thenReturn(matMock);
+		Mockito.when(matMock.getAdditionalRenderState()).thenReturn(renderStateMock);
 	}
 
+	/**
+	 * testing makeObject().
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMakeObject() {
+		assertTrue(yf.makeObject(asmMock, saMock) instanceof Spatial);
+	}
+	
+	/**
+	 * testing makeYetiSnowBall().
+	 */
+	@Test
+	public void testMakeYetiSnowBall() {
+		yf.makeObject(asmMock, saMock);
+		assertTrue(yf.makeYetiSnowBall().getControl(0) instanceof YetiControl);
+	}
+	
+	/**
+	 * testing getyetiSnowBallMaterial().
+	 */
+	@Test
+	public void testGetYetiSnowBallMaterial() {
+		yf.makeObject(asmMock, saMock);
+		final Object mat = yf.getYetiSnowBallMaterial();
+		assertTrue(mat instanceof Material);
+		Mockito.verify(saMock, Mockito.times(2)).getRootNode();
 	}
 
 }
