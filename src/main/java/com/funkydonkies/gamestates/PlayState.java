bebfbd9e83c1d06bd34303e3c125fe915f78fea3
Danilodum14 <D.Dumeljic-1@student.tudelft.nl>
2015-06-16 13:09:05 +0200

    fixed failing tests

--- src/main/java/com/funkydonkies/gamestates/PlayState.java
+++ src/main/java/com/funkydonkies/gamestates/PlayState.java
@@ -47,7 +47,7 @@ public class PlayState extends AbstractAppState {
 		}
 		stateManage = stateManager;
 
-		app.getFlyByCamera().setEnabled(true);
+		app.getFlyByCamera().setEnabled(false);
 		app.getCamera().setLocation(CAM_LOCATION);
 
 		handleBulletAppState();
@@ -69,8 +69,8 @@ public class PlayState extends AbstractAppState {
 		sceneState = new SceneState();
 		stateManage.attach(sceneState);
 		
-//		gameSoundState = new GameBackgroundMusicState();
-//		stateManage.attach(gameSoundState);
+		gameSoundState = new GameBackgroundMusicState();
+		stateManage.attach(gameSoundState);
 		
 		difficultyState = new DifficultyState();
 		stateManage.attach(difficultyState);
